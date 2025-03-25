ef04132132c11fa504a2f99dfd1dc460fa9f4b7c
npatel <namrata.patel@orionik.com>
2025-03-25 16:28:40 +0530

    Custom page size set 50

--- src/pages/Pages/Admin/OurService/OurServiceTable.tsx
+++ src/pages/Pages/Admin/OurService/OurServiceTable.tsx
@@ -513,7 +513,7 @@ const ServiceTable: React.FC = () => {
           data={serviceData}
           isGlobalFilter={false}
           totalItems={selectedTotalItems ?? 0}
-          customPageSize={10}
+          customPageSize={50}
           divClass="table-responsive table-card"
           SearchPlaceholder="Search..."
         />
