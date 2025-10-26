1cefb08ff2eb56d8f085ab43365f3f78ba861c4f
Anjali Prajapati <anjali.prajapati@orionik.com>
2025-10-26 11:33:45 +0530

    Change url in config

--- src/config.ts
+++ src/config.ts
@@ -24,7 +24,7 @@ interface Config {
   commonText: CommonTextConfig;
 }
 // const API_MAIN_URL = "https://shear-brilliance-apiv2.onrender.com";
-const API_MAIN_URL = "https://shearbrilliance-v4-api-payment.onrender.com";
+const API_MAIN_URL = "https://shearbrilliance-v4-api-payment-1.onrender.com";
 const config: Config = {
   google: {
     API_KEY: "",
